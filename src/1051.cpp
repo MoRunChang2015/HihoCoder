753e274e1e36fcd116c9b8033ee75ccbd323e5e6
MoRunChang <709403987@qq.com>
2016-09-12 19:39:12 +0800

    finish 1056

---
+++ src/1051.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    int t, n, m, i, j;
+    int a[102];
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+        for (i = 1; i <= n; i++)
+            cin >> a[i];
+        a[0] = 0;
+        a[n + 1] = 101;
+        int len = min(n, m) + 1;
+        int ans = 0;
+        for (i = len; i <= n + 1; i++) {
+            if (ans < a[i] - a[i - len])
+                ans = a[i] - a[i - len];
+        }
+        cout << ans - 1 << endl;
+    }
+    return 0;
+}
