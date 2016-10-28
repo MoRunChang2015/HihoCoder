7285fb98add695893c45115be197efff3c390c43
MoRunChang <709403987ac@gmail.com>
2016-10-28 19:22:35 +0800

    finish contest12 P1

---
+++ contest/hihoCoder太阁最新面经算法竞赛12/p1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int a[11] = {0};
+int d[5] = { 0 };
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        cin >> d[i];
+        a[d[i]]++;
+    }
+    int h = 23, m = 59;
+    while (h >= 0) {
+        bool flag = true;
+        if (a[h / 10] != 0) {
+            a[h / 10]--;
+        } else {
+            flag = false;
+        }
+        if (flag && a[h % 10] != 0) {
+            a[h % 10]--;
+        } else {
+            flag = false;
+        }
+        if (flag && a[m / 10] != 0) {
+            a[m / 10]--;
+        } else {
+            flag = false;
+        }
+        if (flag && a[m % 10] != 0) {
+            a[m % 10]--;
+        } else {
+            flag = false;
+        }
+        if (flag) {
+            if (h < 10)
+                cout << "0";
+            cout << h << ":";
+            if (m < 10)
+                cout << "0";
+            cout << m << endl;
+            return 0;
+        }
+        memset(a, 0, sizeof a);
+        for (int i = 1; i <= 4; i++)
+            a[d[i]]++;
+        m--;
+        if (m == -1) {
+            h--;
+            m = 59;
+        }
+    }
+    cout << "NOT POSSIBLE" << endl;
+    return 0;
+}
