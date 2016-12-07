f0918e9f7761699042227d22cb59097418204014
MoRunChang <709403987ac@gmail.com>
2016-12-07 11:41:15 +0800

    try 1283

---
+++ src/1283.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int a[1020];
+int main() {
+    int n, i;
+    int ans = 0;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+        cin >> a[i];
+    i = n;
+    while (a[i - 1] < a[i] && i > 2)
+        i--;
+    i--;
+    for (int j = 1; j < i; j++)
+        cout << a[j] << " ";
+    cout << a[i] << endl;
+    return 0;
+}
